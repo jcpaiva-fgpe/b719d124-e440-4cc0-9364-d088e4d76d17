1d749e83b5e7f318942fb1a1c42181c41361a885
José Carlos Paiva <jose.c.paiva@inesctec.pt>
2021-07-09 18:29:55 +0100

    updated file exercises/db6a4f54-2aeb-4502-823a-92914aa8e60b/templates/390fc816-4d17-4cd8-b0c5-9903f33f7895/template.js

--- exercises/db6a4f54-2aeb-4502-823a-92914aa8e60b/templates/390fc816-4d17-4cd8-b0c5-9903f33f7895/template.js
+++ exercises/db6a4f54-2aeb-4502-823a-92914aa8e60b/templates/390fc816-4d17-4cd8-b0c5-9903f33f7895/template.js
@@ -6,7 +6,7 @@ function main() {
     let ln;
     while((ln = readln()).indexOf(' ') >= 0) {
         const [s, ws] = ln.split(' ');
-        const addS = addSuffix(s);
+        const addS = add_suffix(s);
         ws.forEach(w => writeln(addS(w)));
     }
 }
